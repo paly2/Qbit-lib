f268c8066988b98ff23076959076e7f8a21847cc
palige <plangrognet@laposte.net>
2016-04-30 17:23:29 +0200

    Change a bit qbit.h, add back the empty constructor

--- qbit.h
+++ qbit.h
@@ -8,12 +8,13 @@ typedef vector<STATE> vState; // STATE vector (descriptor of a qbit)
 
 class Qbit {
 	public:
+	Qbit(){} // Creates an empty qbit
 	Qbit(STATE); // Creates a qbit with a first classic state (recommanded).
-	Qbit(vector<STATE>); // Creates a quantum qbit with vState descriptor
+	Qbit(vState); // Creates a quantum qbit with vState descriptor
 	
-	void set_state(vector<STATE> new_state); // Use this function only if you know what you are doing.
+	void set_state(vState new_state); // Use this function only if you know what you are doing.
+	vState get_state() const; // Returns the list of superposed states of qbit.
 	
-	vector<STATE> get_state() const; // Returns the list of superposed states of qbit.
 	void add_state(STATE new_state); // Superposes another state.
 	
 	STATE read() const; // Reads qbit (returns random state in superposed states).
@@ -27,7 +28,7 @@ class Qbit {
 	bool check(bool (*function)(STATE)); // Check the return of a function for all qbit, returns true if the function returns always true.
 	
 	private:
-	vector<STATE> state;
+	vState state;
 };
 
 ostream& operator<<(ostream &stream, const Qbit& qbit); // Pushes a Qbit into a flush
